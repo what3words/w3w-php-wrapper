aa127b23df17721e82449d6e4e180754ba3d305f
vicchi <gary@vicchi.org>
2016-05-12 13:04:52 +0100

    Rewrite/restructure for v2 API, including basic phpUnit testing

---
+++ tests/GeocoderTest.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * w3w-php-wrapper - A PHP library to use the what3words RESTful API
+ *
+ * @author Gary Gale <gary@what3words.com>
+ * @copyright 2016 what3words Ltd
+ * @link http://developer.what3words.com
+ * @license MIT
+ * @version 2.0.0
+ * @package What3words\Geocoder
+ */
+
+namespace What3words\Geocoder\Test;
+
+use What3words\Geocoder\Geocoder;
+
+class GeocoderTest extends \PHPUnit_Framework_TestCase {
+    protected $geocoder;
+
+    protected function setUp() {
+        $options = [
+            'key' => API_KEY
+        ];
+        $this->geocoder = new Geocoder($options);
+    }
+
+    public function testBuildGeocoder() {
+        $this->assertObjectHasAttribute('key', $this->geocoder);
+        $this->assertObjectHasAttribute('timeout', $this->geocoder);
+    }
+
+    public function testInvalidKey() {
+        $options = [
+            'key' => 'NOTWORKING'
+        ];
+        $geocoder = new Geocoder($options);
+        $threeWordAddr = 'index.home.raft';
+        $payload = $geocoder->forwardGeocode($threeWordAddr);
+        $json = json_decode($payload, true);
+
+        $expected = [
+            'code' => 2
+        ];
+        $this->assertArraySubset($expected, $json);
+    }
+
+    public function testForwardGeocodeWithJson() {
+        $threeWordAddr = 'index.home.raft';
+        $payload = $this->geocoder->forwardGeocode($threeWordAddr);
+
+        $json = json_decode($payload, true);
+        $expected = [
+            'status' => [
+                'status' => 200,
+                'reason' => 'OK'
+            ],
+            'words' => 'index.home.raft',
+            'geometry' => [
+                'lat' => 51.521251,
+                'lng' => -0.203586
+            ]
+        ];
+        $this->assertArraySubset($expected, $json);
+    }
+
+    public function testReverseGeocodeWithJson() {
+        $coords = [
+            'lat' => 51.521251,
+            'lng' => -0.203586
+        ];
+        $payload = $this->geocoder->reverseGeocode($coords);
+        $json = json_decode($payload, true);
+        $expected = [
+            'status' => [
+                'status' => 200,
+                'reason' => 'OK'
+            ],
+            'words' => 'index.home.raft',
+            'geometry' => [
+                'lat' => 51.521251,
+                'lng' => -0.203586
+            ]
+        ];
+        $this->assertArraySubset($expected, $json);
+    }
+}
+
+?>
---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * w3w-php-wrapper - A PHP library to use the what3words RESTful API
+ *
+ * @author Gary Gale <gary@what3words.com>
+ * @copyright 2016 what3words Ltd
+ * @link http://developer.what3words.com
+ * @license MIT
+ * @version 2.0.0
+ * @package What3words\Geocoder
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+define('API_KEY', getenv('W3W_API_KEY'));
+
+?>
