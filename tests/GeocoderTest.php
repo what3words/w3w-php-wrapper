5d987efdb6082e062b691366dd6046280e527e0c
Victor Lap <victorlap@outlook.com>
2025-10-21 23:08:40 +0200

    Update to php >= 7.4

--- tests/GeocoderTest.php
+++ tests/GeocoderTest.php
@@ -23,7 +23,7 @@ class GeocoderTest extends TestCase
 {
     protected $geocoder;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->geocoder = new Geocoder(getenv('W3W_API_KEY'));
     }
